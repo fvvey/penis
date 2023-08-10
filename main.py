5eeda730eb3d3ad0bf99f503f30a980dc5ca3d40
fvvey <141887576+fvvey@users.noreply.github.com>
2023-08-09 20:43:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def print_results(results: list, time_taken: float):
     system('cls')
     print("".join(i+"\n" for i in results))
     print(f"THE TIME THIS SHIT SHOULD TAKE: {ratelimit_multi}\n"
-          f"TIME THIS STUPID SHIT TOOK (PROBABLY SLOW AS HELL): {round(time_taken, 4)}s")
+          f"TIME THIS STUPID SHIT TOOK (PROBABLY SLOW AS HELL NO CAP): {round(time_taken, 4)}s")
 
 
 # The main function that checks an asset and snipes if possible.
